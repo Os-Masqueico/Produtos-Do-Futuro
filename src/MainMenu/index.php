c6ad3a75fcc48ca6247947a0eee4741cbad5939b
PewDizinho <konopkspaulo046@gmail.com>
2024-06-02 20:21:40 -0300

    some init stuff

---
+++ src/MainMenu/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include_once '../util/config.php';
+    echo '<title>' . SITE_NAME . '</title>';
+    echo '<link rel="icon" href="' . SITE_LOGO . '">';
+    ?>
+</head>
+
+<body>
+    <?php
+    echo NAV_BAR;
+    ?>
+
+</body>
+
+
+</html>
---
+++ src/util/config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PATH', 'http://localhost/Produtos-Do-Futuro/src/');
+define('CSS_PATH', ROOT_PATH . 'css/');
+define('JS_PATH', ROOT_PATH . 'js/');
+define('IMG_PATH', ROOT_PATH . 'assets/');
+
+define('SITE_NAME', 'Produtos do Futuro');
+define('SITE_LOGO', IMG_PATH . 'logo/logo.png');
+define('NAV_BAR', file_get_contents(ROOT_PATH . 'util/global/navbar.html'));
